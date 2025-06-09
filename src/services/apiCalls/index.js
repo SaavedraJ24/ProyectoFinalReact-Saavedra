8b5f3f1399427aadcf7f627fa3b9094ed1b773e7
SaavedraJ24 <joaquinsaavedraesc@gmail.com>
2025-06-08 21:25:59 -0300

    Add api request for getCategories

--- src/services/apiCalls/index.js
+++ src/services/apiCalls/index.js
@@ -2,3 +2,5 @@ export * from './getProducts';
 export * from './addProduct';
 export * from './updateProduct';
 export * from './deleteProduct';
+export * from './getCategories';
+export * from './searchProduct';
