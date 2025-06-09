a9a3150ab665277313cfd47deb63a4f94bf62017
SaavedraJ24 <joaquinsaavedraesc@gmail.com>
2025-06-08 21:52:40 -0300

    Add hook folder to project

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,1 +1,5 @@
 export * from "./useAuth";
+export * from "./useProducts";
+export * from "./useSearch";
+export * from "./useCategories";
+export * from "./useCustomHProductsStore";
---
+++ src/hooks/useProducts.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+import { getProducts, getProductsByCategory, getProductById } from "../services"
+
+export const useProducts = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getProducts().then((res) => {
+            setProducts(res.data.products);
+        }).catch((error) => {
+            console.error(error)
+        }).finally(() => setLoading(false));
+    }, []);
+
+    return { products, loading };
+
+}
+
+export const useProductsByCategory = (id) => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getProductsByCategory(id).then((res) => {
+            setProducts(res.data.products);
+        }).catch((error) => {
+            console.error(error)
+        }).finally(() => setLoading(false));
+    }, [id]);
+
+    return { products, loading };
+
+}
+
+export const useProductById = (id) => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getProductById(id).then((res) => {
+            setProducts(res.data.products);
+        }).catch((error) => {
+            console.error(error)
+        }).finally(() => setLoading(false));
+    }, []);
+
+    return { products, loading };
+
+}
