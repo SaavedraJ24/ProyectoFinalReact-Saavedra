792331cac024eaa56470f5a126665520c6fba7da
SaavedraJ24 <joaquinsaavedraesc@gmail.com>
2025-06-20 22:38:48 -0300

    rutas protegidas

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,40 +1,55 @@
-import { createBrowserRouter } from "react-router";
-import { Login, Signup } from "../pages";
+import { createBrowserRouter } from "react-router-dom";
+import {
+  Cart,
+  Category,
+  Checkout,
+  Home,
+  Item,
+  Login,
+  Signup,
+} from "../pages";
+import { MainLayout } from "../layout/MainLayout";
 import { ProtectedRoute } from "../components";
-import { MainLayout } from "../layout";
+
 const routes = [
-    {
-        path: "./",
-        element: (
-        <ProtectedRoute>
-            <MainLayout/>,
-        </ProtectedRoute>),
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-            {
-                path: "/category/:id",
-                element: <Category />,
-            },
-            {
-                path: "/item/:id",
-                element: <Item />,
-            },
-            {
-                path: "/cart",
-                element: <Cart />,
-            },
-        ],
-    },
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/signup",
-        element: <Signup />,
-    },
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <MainLayout />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/category/:id",
+        element: <Category />,
+      },
+      {
+        path: "/item/:id",
+        element: <Item />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/checkout",
+        element: <Checkout />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
 ];
+
 export const router = createBrowserRouter(routes);
